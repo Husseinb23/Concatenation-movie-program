536ee22fe044d22960934a6e91890fd9a990c242
Hussein Barakat <121978208+HBarakatcode23@users.noreply.github.com>
2023-01-25 23:01:47 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+AMovie = input("Name a movie: ")
+aSnack = input("Name a type of snack: ")
+aTheater = input("Name a movie theater: ")
+aCity = input("Name a city: ")
+print("You can't wait to see",AMovie, "while eating", aSnack, "In a theater called", aTheater, "watching it in", aCity)
